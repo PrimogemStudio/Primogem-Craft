cc81c0eb24459073fda6bbd82c5eada238f1a5ce
Pepper MR <zheshixbde@outlook.com>
2024-04-11 16:16:22 +0800

    完善奇物龋齿模型
    仅功能

--- src/main/java/net/mcreator/ceshi/item/QwqcxxmxItem.java
+++ src/main/java/net/mcreator/ceshi/item/QwqcxxmxItem.java
@@ -24,11 +24,14 @@ public class QwqcxxmxItem extends Item {
 	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, level, list, flag);
 		list.add(Component.literal("\u00A72\u53F3\u952E\u540E\u6D88\u8017\u6240\u6709\u5B87\u5B99\u788E\u7247\u83B7\u5F97"));
-		list.add(Component.literal("\u00A72\u529B\u91CF\u6548\u679C"));
-		list.add(Component.literal("\u00A77\u529B\u91CF\u7B49\u7EA7=\u5B87\u5B99\u788E\u7247\u9664\u4EE516"));
-		list.add(Component.literal("\u00A77\u521D\u59CB\u6301\u7EED1\u5206\u949F"));
-		list.add(Component.literal("\u00A76\u6F5C\u884C+\u53F3\u952E\u6BCF\u6B21\u6D88\u8017\u6700\u5927\u6301\u7EED\u65F6\u95F4\u6570\u91CF\u7684"));
-		list.add(Component.literal("\u00A76\u5B87\u5B99\u788E\u7247\u6765\u63D0\u53471\u79D2\u6700\u5927\u6301\u7EED\u65F6\u95F4"));
+		list.add(Component.literal("\u00A72 \u529B\u91CF\u6548\u679C"));
+		list.add(Component.literal("\u00A76\u529B\u91CF\u7B49\u7EA7\u00A7b=\u00A76\u5B87\u5B99\u788E\u7247\u6570\u91CF\u00A7b\\\u00A7616"));
+		list.add(Component.literal("\u00A75 \u521D\u59CB\u6301\u7EED1\u5206\u949F"));
+		list.add(Component.literal("\u00A76\u6F5C\u884C+\u53F3\u952E\u6BCF\u6B21\u6D88\u8017\u6700\u5927\u6301\u7EED"));
+		list.add(Component.literal("\u00A76 \u5206\u949F\u6570\u91CF\u5B87\u5B99\u788E\u7247\u6765\u63D0\u5347\u00A7e1\u00A76\u79D2"));
+		list.add(Component.literal("\u00A76 \u6700\u5927\u6301\u7EED\u65F6\u95F4"));
+		list.add(Component.literal("\u00A78\u6BCF\u6B21\u4F7F\u7528\u6263\u9664\u6548\u679C\u7B49\u7EA7*5\u6570\u91CF"));
+		list.add(Component.literal("\u00A78 \u6700\u5927\u6301\u7EED\u65F6\u95F4"));
 	}
 
 	@Override
--- src/main/java/net/mcreator/ceshi/procedures/Qcmx_sxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Qcmx_sxProcedure.java
@@ -25,10 +25,21 @@ public class Qcmx_sxProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
+		ItemStack c = ItemStack.EMPTY;
 		double a = 0;
 		double b = 0;
-		if (!entity.isShiftKeyDown()) {
-			if (!itemstack.getOrCreateTag().getBoolean("quchi_sx")) {
+		double s1 = 0;
+		double s2 = 0;
+		if (!world.isClientSide() && (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get())) : false)) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 1, 1, false);
+				}
+			}
+			if (!itemstack.getOrCreateTag().getBoolean("quchichushi")) {
+				itemstack.getOrCreateTag().putBoolean("quchichushi", true);
 				itemstack.getOrCreateTag().putDouble("quchi_sx", 60);
 			}
 			{
@@ -43,35 +54,52 @@ public class Qcmx_sxProcedure {
 					}
 				}
 			}
-			if (entity instanceof Player _player) {
-				ItemStack _stktoremove = new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get());
-				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), (int) a, _player.inventoryMenu.getCraftSlots());
-			}
-			if (a != 0) {
-				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-					_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, (int) (itemstack.getOrCreateTag().getDouble("quchi_sx") * 20), (int) (a * 0.0625 - 1), false, false));
-			}
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 1, 1);
-				} else {
-					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 1, 1, false);
-				}
-			}
-		} else {
-			if (!itemstack.getOrCreateTag().getBoolean("quchi_sx")) {
-				itemstack.getOrCreateTag().putDouble("quchi_sx", 60);
-			}
-			b = Math.round(itemstack.getOrCreateTag().getDouble("quchi_sx") * 0.016);
-			if (a >= b) {
+			if (!entity.isShiftKeyDown()) {
 				if (entity instanceof Player _player) {
 					ItemStack _stktoremove = new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get());
-					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), (int) b, _player.inventoryMenu.getCraftSlots());
+					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), (int) a, _player.inventoryMenu.getCraftSlots());
 				}
-				itemstack.getOrCreateTag().putDouble("quchi_sx", (itemstack.getOrCreateTag().getDouble("quchi_sx") + 1));
-				itemstack.setHoverName(Component.literal(("\u00A75\u300E\u5947\u7269\u300F\u00A7f\u9F8B\u9F7F\u661F\u7CFB\u6A21\u578B" + new java.text.DecimalFormat("\u00A7b##.s##").format(itemstack.getOrCreateTag().getDouble("quchi_sx")))));
+				if (a != 0) {
+					if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+						_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, (int) (itemstack.getOrCreateTag().getDouble("quchi_sx") * 20), (int) (a * (1 / 16.0) - 1), false, false));
+					itemstack.getOrCreateTag().putDouble("quchi_sx", (itemstack.getOrCreateTag().getDouble("quchi_sx") - Math.floor(a * (1 / 16.0)) * 5));
+					if (itemstack.getOrCreateTag().getDouble("quchi_sx") < 1) {
+						c = itemstack;
+						if (entity instanceof Player _player) {
+							ItemStack _stktoremove = c;
+							_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+						}
+						if (entity instanceof Player _player && !_player.level().isClientSide())
+							_player.displayClientMessage(Component.literal("\u00A75\u300E\u5947\u7269\u300F\u00A7f\u9F8B\u9F7F\u661F\u7CFB\u6A21\u578B\u00A7c\u5DF2\u635F\u574F\uFF01"), false);
+					}
+				}
+				if (entity instanceof Player _player && !_player.level().isClientSide())
+					_player.displayClientMessage(Component.literal("\u00A77\u6D88\u8017\u4E86\u6240\u6709\u5B87\u5B99\u788E\u7247\u6362\u53D6\u4E86\u529B\u91CF\u6548\u679C"), false);
+			} else {
+				b = Math.floor(itemstack.getOrCreateTag().getDouble("quchi_sx") * (1 / 60.0));
+				if (a >= b) {
+					if (entity instanceof Player _player) {
+						ItemStack _stktoremove = new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get());
+						_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), (int) b, _player.inventoryMenu.getCraftSlots());
+					}
+					itemstack.getOrCreateTag().putDouble("quchi_sx", (itemstack.getOrCreateTag().getDouble("quchi_sx") + 1));
+				}
 			}
+			itemstack
+					.setHoverName(
+							Component
+									.literal(
+											("\u00A75\u300E\u5947\u7269\u300F\u00A7f\u9F8B\u9F7F\u661F\u7CFB\u6A21\u578B"
+													+ (itemstack.getOrCreateTag().getDouble("quchi_sx") < 1
+															? "\u00A7c0s"
+															: "" + (itemstack.getOrCreateTag().getDouble("quchi_sx") >= 60
+																	? new java.text.DecimalFormat("\u00A7b##.m##").format(Math.floor(itemstack.getOrCreateTag().getDouble("quchi_sx") * (1 / 60.0))) + ""
+																			+ new java.text.DecimalFormat("##.s##")
+																					.format(itemstack.getOrCreateTag().getDouble("quchi_sx") - Math.floor(itemstack.getOrCreateTag().getDouble("quchi_sx") * (1 / 60.0)) * 60)
+																	: new java.text.DecimalFormat("\u00A7b##.s##").format(itemstack.getOrCreateTag().getDouble("quchi_sx")))))));
 		}
 		DiaoyonghuishouProcedure.execute(entity, itemstack);
+		if (entity instanceof Player _player && !_player.level().isClientSide())
+			_player.displayClientMessage(Component.literal((new java.text.DecimalFormat("##.##").format(itemstack.getOrCreateTag().getDouble("quchi_sx")))), false);
 	}
 }
