d69727e1663455800b16b4eff8b804eba9e0bbf2
Pepper MR <zheshixbde@outlook.com>
2024-07-10 17:18:16 +0800

    修复事件右键触发

--- src/main/java/net/mcreator/ceshi/item/SHJwupinItem.java
+++ src/main/java/net/mcreator/ceshi/item/SHJwupinItem.java
@@ -1,25 +1,11 @@
 
 package net.mcreator.ceshi.item;
 
-import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
-
-import net.mcreator.ceshi.procedures.SJwpsxProcedure;
 
 public class SHJwupinItem extends Item {
 	public SHJwupinItem() {
 		super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.RARE));
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-		SJwpsxProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity, ar.getObject());
-		return ar;
 	}
 }
