450206ed6eacf1b57401110d4a59080900a221cd
Pepper MR <zheshixbde@outlook.com>
2024-07-15 02:06:37 +0800

    设置禁用储物囊规则在服务器默认启动

---
+++ src/main/java/net/mcreator/ceshi/procedures/JinyongnangmorenProcedure.java
@@ -0,0 +1,30 @@
+package net.mcreator.ceshi.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+
+import net.mcreator.ceshi.init.PrimogemcraftModGameRules;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class JinyongnangmorenProcedure {
+	@SubscribeEvent
+	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		execute(event, event.getEntity().level());
+	}
+
+	public static void execute(LevelAccessor world) {
+		execute(null, world);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world) {
+		if (world.isClientSide() && world.getLevelData().getGameRules().getBoolean(PrimogemcraftModGameRules.JINYONGNANG)) {
+			world.getLevelData().getGameRules().getRule(PrimogemcraftModGameRules.JINYONGNANG).set(true, world.getServer());
+		}
+	}
+}
