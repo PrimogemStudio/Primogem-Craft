2d9655dbee0ad271cdde61f7bd09e921f7008298
Pepper MR <zheshixbde@outlook.com>
2024-07-21 14:40:09 +0800

    新物品可食用

--- src/main/java/net/mcreator/ceshi/item/YsjfrItem.java
+++ src/main/java/net/mcreator/ceshi/item/YsjfrItem.java
@@ -6,13 +6,14 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.network.chat.Component;
 
 import java.util.List;
 
 public class YsjfrItem extends Item {
 	public YsjfrItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(1).saturationMod(0.1f).meat().build()));
 	}
 
 	@Override
--- src/main/java/net/mcreator/ceshi/item/YsspItem.java
+++ src/main/java/net/mcreator/ceshi/item/YsspItem.java
@@ -3,9 +3,10 @@ package net.mcreator.ceshi.item;
 
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
 
 public class YsspItem extends Item {
 	public YsspItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.2f).alwaysEat().build()));
 	}
 }
