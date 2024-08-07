0e215d2ed0d5965e0188448c85de517e989b354b
Pepper MR <zheshixbde@outlook.com>
2024-08-08 01:28:15 +0800

    祈愿归属修复

--- src/main/java/net/mcreator/ceshi/procedures/TtiaoguozhizhangshuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/TtiaoguozhizhangshuxingProcedure.java
@@ -42,55 +42,56 @@ public class TtiaoguozhizhangshuxingProcedure {
 				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(16 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 				for (Entity entityiterator : _entfound) {
 					if (entityiterator instanceof QQyuanchuzi01Entity || entityiterator instanceof QQQyuanchulan01Entity || entityiterator instanceof QqiyuanJinGuangEntity) {
-						if (!(entityiterator.getPersistentData().getString("qiyuan_guishu")).equals(entity.getDisplayName().getString())) {
-							if (entity instanceof Player _player && !_player.level().isClientSide())
-								_player.displayClientMessage(Component.literal("\u00A76\u5305\u542B\u4E00\u4E9B\u4E0D\u5C5E\u4E8E\u4F60\u7684\u7948\u613F\u7ED3\u679C\uFF0C\u5B83\u5C5E\u4E8E\u5176\u4ED6\u4EBA"), false);
-						}
-						if (!(entityiterator instanceof LivingEntity _livEnt7 && _livEnt7.hasEffect(PrimogemcraftModMobEffects.DJQJKJXGXIANZHI.get()))) {
-							if (world instanceof Level _level) {
-								if (!_level.isClientSide()) {
-									_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:dashengchulan01")), SoundSource.PLAYERS, (float) 0.2, (float) 0.9);
-								} else {
-									_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:dashengchulan01")), SoundSource.PLAYERS, (float) 0.2, (float) 0.9, false);
-								}
-							}
-							if (entityiterator instanceof QqiyuanJinGuangEntity && entityiterator.getPersistentData().getBoolean("chouka_jiance_2")) {
-								if (world instanceof ServerLevel _level)
-									_level.getServer().getCommands().performPrefixedCommand(
-											new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
-													.withSuppressedOutput(),
-											"loot spawn ~ ~ ~ loot primogemcraft:qqyjin");
-							}
-							if (entityiterator instanceof QQyuanchuzi01Entity && entityiterator.getPersistentData().getBoolean("chouka_jiance_1")) {
-								if (world instanceof ServerLevel _level)
-									_level.getServer().getCommands().performPrefixedCommand(
-											new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
-													.withSuppressedOutput(),
-											"loot spawn ~ ~ ~ loot primogemcraft:q_qqyzi");
-							}
-							if (entityiterator instanceof QQQyuanchulan01Entity && entityiterator.getPersistentData().getBoolean("chouka_jiance_0")) {
-								if (world instanceof ServerLevel _level)
-									_level.getServer().getCommands().performPrefixedCommand(
-											new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
-													.withSuppressedOutput(),
-											"loot spawn ~ ~ ~ loot primogemcraft:qq_qqylan");
-							}
-							if (!(entityiterator.getPersistentData().getBoolean("chouka_jiance_2") || entityiterator.getPersistentData().getBoolean("chouka_jiance_1") || entityiterator.getPersistentData().getBoolean("chouka_jiance_0"))) {
-								if (world instanceof ServerLevel _level) {
-									ItemEntity entityToSpawn = new ItemEntity(_level, (entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ()), new ItemStack(PrimogemcraftModItems.YSJFR.get()));
-									entityToSpawn.setPickUpDelay(10);
-									_level.addFreshEntity(entityToSpawn);
-								}
+						if (!(entityiterator instanceof LivingEntity _livEnt4 && _livEnt4.hasEffect(PrimogemcraftModMobEffects.DJQJKJXGXIANZHI.get()))) {
+							if (!(entityiterator.getPersistentData().getString("qiyuan_guishu")).equals(entity.getDisplayName().getString())) {
 								if (entity instanceof Player _player && !_player.level().isClientSide())
-									_player.displayClientMessage(Component.literal("\u00A7c\u68C0\u6D4B\u5230\u4E0D\u5E94\u8BE5\u5B58\u5728\u7684\u62BD\u5361\u5B9E\u4F53\uFF0C\u5DF2\u5C06\u5176\u9500\u6BC1"), false);
-							}
-							if (!entityiterator.level().isClientSide())
-								entityiterator.discard();
-							{
-								ItemStack _ist = itemstack;
-								if (_ist.hurt(1, RandomSource.create(), null)) {
-									_ist.shrink(1);
-									_ist.setDamageValue(0);
+									_player.displayClientMessage(Component.literal("\u00A76\u5305\u542B\u4E00\u4E9B\u4E0D\u5C5E\u4E8E\u4F60\u7684\u7948\u613F\u7ED3\u679C\uFF0C\u5B83\u5C5E\u4E8E\u5176\u4ED6\u4EBA"), false);
+							} else {
+								if (world instanceof Level _level) {
+									if (!_level.isClientSide()) {
+										_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:dashengchulan01")), SoundSource.PLAYERS, (float) 0.2, (float) 0.9);
+									} else {
+										_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("primogemcraft:dashengchulan01")), SoundSource.PLAYERS, (float) 0.2, (float) 0.9, false);
+									}
+								}
+								if (entityiterator instanceof QqiyuanJinGuangEntity && entityiterator.getPersistentData().getBoolean("chouka_jiance_2")) {
+									if (world instanceof ServerLevel _level)
+										_level.getServer().getCommands().performPrefixedCommand(
+												new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
+														.withSuppressedOutput(),
+												"loot spawn ~ ~ ~ loot primogemcraft:qqyjin");
+								}
+								if (entityiterator instanceof QQyuanchuzi01Entity && entityiterator.getPersistentData().getBoolean("chouka_jiance_1")) {
+									if (world instanceof ServerLevel _level)
+										_level.getServer().getCommands().performPrefixedCommand(
+												new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
+														.withSuppressedOutput(),
+												"loot spawn ~ ~ ~ loot primogemcraft:q_qqyzi");
+								}
+								if (entityiterator instanceof QQQyuanchulan01Entity && entityiterator.getPersistentData().getBoolean("chouka_jiance_0")) {
+									if (world instanceof ServerLevel _level)
+										_level.getServer().getCommands().performPrefixedCommand(
+												new CommandSourceStack(CommandSource.NULL, new Vec3((entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ())), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null)
+														.withSuppressedOutput(),
+												"loot spawn ~ ~ ~ loot primogemcraft:qq_qqylan");
+								}
+								if (!(entityiterator.getPersistentData().getBoolean("chouka_jiance_2") || entityiterator.getPersistentData().getBoolean("chouka_jiance_1") || entityiterator.getPersistentData().getBoolean("chouka_jiance_0"))) {
+									if (world instanceof ServerLevel _level) {
+										ItemEntity entityToSpawn = new ItemEntity(_level, (entityiterator.getX()), (entityiterator.getY()), (entityiterator.getZ()), new ItemStack(PrimogemcraftModItems.YSJFR.get()));
+										entityToSpawn.setPickUpDelay(10);
+										_level.addFreshEntity(entityToSpawn);
+									}
+									if (entity instanceof Player _player && !_player.level().isClientSide())
+										_player.displayClientMessage(Component.literal("\u00A7c\u68C0\u6D4B\u5230\u4E0D\u5E94\u8BE5\u5B58\u5728\u7684\u62BD\u5361\u5B9E\u4F53\uFF0C\u5DF2\u5C06\u5176\u9500\u6BC1"), false);
+								}
+								if (!entityiterator.level().isClientSide())
+									entityiterator.discard();
+								{
+									ItemStack _ist = itemstack;
+									if (_ist.hurt(1, RandomSource.create(), null)) {
+										_ist.shrink(1);
+										_ist.setDamageValue(0);
+									}
 								}
 							}
 						}
