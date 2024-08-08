9348695dae75497ea422e48f354a7af5aea230b2
Pepper MR <zheshixbde@outlook.com>
2024-08-08 16:42:31 +0800

    修改战利品表

--- src/main/java/net/mcreator/ceshi/procedures/JiuchanzhigaosuijidiaoluoProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/JiuchanzhigaosuijidiaoluoProcedure.java
@@ -5,15 +5,18 @@ import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.BlockPos;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
 
 public class JiuchanzhigaosuijidiaoluoProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (Math.random() < 0.2) {
-			if (world instanceof ServerLevel _level)
-				_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-						"loot spawn ~ ~1 ~ loot primogemcraft:blocks/jiuchanzhiyuangaozhanlipin");
+		if (!world.isClientSide()) {
+			if (Math.random() < 0.3 && world.getBlockState(BlockPos.containing(x, y, z)).canOcclude()) {
+				if (world instanceof ServerLevel _level)
+					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+							"loot spawn ~ ~1 ~ loot primogemcraft:blocks/jiuchanzhiyuangaozhanlipin");
+			}
 		}
 	}
 }
--- src/main/java/net/mcreator/ceshi/procedures/PpohuaifangkuaisuijimolaProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/PpohuaifangkuaisuijimolaProcedure.java
@@ -4,28 +4,33 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.BlockPos;
 
 import net.mcreator.ceshi.init.PrimogemcraftModItems;
 
 public class PpohuaifangkuaisuijimolaProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (Math.random() < 0.05) {
-			if (world instanceof ServerLevel _level) {
-				ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
-				entityToSpawn.setPickUpDelay(10);
-				_level.addFreshEntity(entityToSpawn);
-			}
-			if (world instanceof ServerLevel _level) {
-				ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
-				entityToSpawn.setPickUpDelay(10);
-				_level.addFreshEntity(entityToSpawn);
-			}
-		} else {
-			if (Math.random() < 0.06) {
-				if (world instanceof ServerLevel _level) {
-					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
-					entityToSpawn.setPickUpDelay(10);
-					_level.addFreshEntity(entityToSpawn);
+		if (!world.isClientSide()) {
+			if (world.getBlockState(BlockPos.containing(x, y, z)).canOcclude()) {
+				if (Math.random() < 0.05) {
+					if (world instanceof ServerLevel _level) {
+						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
+						entityToSpawn.setPickUpDelay(10);
+						_level.addFreshEntity(entityToSpawn);
+					}
+					if (world instanceof ServerLevel _level) {
+						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
+						entityToSpawn.setPickUpDelay(10);
+						_level.addFreshEntity(entityToSpawn);
+					}
+				} else {
+					if (Math.random() < 0.06) {
+						if (world instanceof ServerLevel _level) {
+							ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.MMOLA_01.get()));
+							entityToSpawn.setPickUpDelay(10);
+							_level.addFreshEntity(entityToSpawn);
+						}
+					}
 				}
 			}
 		}
