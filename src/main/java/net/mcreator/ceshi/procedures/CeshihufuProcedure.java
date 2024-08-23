454a12d16ded6a906b131885f6780d3541f31d15
Pepper MR <zheshixbde@outlook.com>
2024-08-24 00:56:42 +0800

    修改三月祝福触发逻辑
    更新饰品tag

--- src/main/java/net/mcreator/ceshi/procedures/CeshihufuProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/CeshihufuProcedure.java
@@ -1,17 +1,10 @@
 package net.mcreator.ceshi.procedures;
 
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.util.RandomSource;
 
-import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
-
 public class CeshihufuProcedure {
-	public static void execute(Entity entity, ItemStack itemstack) {
-		if (entity == null)
-			return;
+	public static void execute(ItemStack itemstack) {
 		itemstack.getOrCreateTag().putDouble("sanyuezhufu", (itemstack.getOrCreateTag().getDouble("sanyuezhufu") + 1));
 		if (itemstack.getOrCreateTag().getDouble("sanyuezhufu") >= 24000) {
 			{
@@ -22,10 +15,6 @@ public class CeshihufuProcedure {
 				}
 			}
 			itemstack.getOrCreateTag().putDouble("sanyuezhufu", 0);
-		}
-		if (!(entity instanceof LivingEntity _livEnt10 && _livEnt10.hasEffect(PrimogemcraftModMobEffects.SYZF.get()))) {
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.SYZF.get(), 60, 0, false, false));
 		}
 	}
 }
--- src/main/java/net/mcreator/ceshi/procedures/SanyuezhufushuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/SanyuezhufushuxingProcedure.java
@@ -1,40 +1,56 @@
 package net.mcreator.ceshi.procedures;
 
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
 
-import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
+import net.mcreator.ceshi.init.PrimogemcraftModItems;
 
 import javax.annotation.Nullable;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @Mod.EventBusSubscriber
 public class SanyuezhufushuxingProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingAttackEvent event) {
 		if (event != null && event.getEntity() != null) {
-			execute(event, event.getSource(), event.getEntity());
+			execute(event, event.getEntity().level(), event.getSource(), event.getEntity());
 		}
 	}
 
-	public static void execute(DamageSource damagesource, Entity entity) {
-		execute(null, damagesource, entity);
+	public static void execute(LevelAccessor world, DamageSource damagesource, Entity entity) {
+		execute(null, world, damagesource, entity);
 	}
 
-	private static void execute(@Nullable Event event, DamageSource damagesource, Entity entity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, DamageSource damagesource, Entity entity) {
 		if (damagesource == null || entity == null)
 			return;
-		if (damagesource.is(DamageTypes.FALL) && entity instanceof LivingEntity _livEnt1 && _livEnt1.hasEffect(PrimogemcraftModMobEffects.SYZF.get())) {
-			if (event != null && event.isCancelable()) {
-				event.setCanceled(true);
-			} else if (event != null && event.hasResult()) {
-				event.setResult(Event.Result.DENY);
+		if (damagesource.is(DamageTypes.FALL)) {
+			{
+				AtomicReference<IItemHandler> _iitemhandlerref = new AtomicReference<>();
+				entity.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(_iitemhandlerref::set);
+				if (_iitemhandlerref.get() != null) {
+					for (int _idx = 0; _idx < _iitemhandlerref.get().getSlots(); _idx++) {
+						ItemStack itemstackiterator = _iitemhandlerref.get().getStackInSlot(_idx).copy();
+						if (itemstackiterator.getItem() == PrimogemcraftModItems.SANYUEZHUFU.get()) {
+							if (event != null && event.isCancelable()) {
+								event.setCanceled(true);
+							} else if (event != null && event.hasResult()) {
+								event.setResult(Event.Result.DENY);
+							}
+						}
+					}
+				}
 			}
 		}
 	}
