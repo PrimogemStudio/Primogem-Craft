46774e285642ec2d3d4cbafc29c0ee0c8aac35b9
Pepper MR <zheshixbde@outlook.com>
2024-08-24 00:32:06 +0800

    回退伤害原实体修改，补全天空属性

--- src/main/java/net/mcreator/ceshi/procedures/TiankongsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/TiankongsxProcedure.java
@@ -1,10 +1,5 @@
 package net.mcreator.ceshi.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
@@ -22,68 +17,41 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.BlockPos;
 
-import javax.annotation.Nullable;
+import net.mcreator.ceshi.PrimogemcraftMod;
 
-@Mod.EventBusSubscriber
 public class TiankongsxProcedure {
-	@SubscribeEvent
-	public static void onEntityAttacked(LivingAttackEvent event) {
-		if (event != null && event.getEntity() != null) {
-			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity(), event.getSource().getEntity());
-		}
-	}
-
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
-		execute(null, world, x, y, z, entity, sourceentity);
-	}
-
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
 		if (!world.isClientSide()) {
 			if (entity.isAlive()) {
 				if (((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrCreateTag().getString("GunId")).equals("pgfs:tiankong")) {
-					if (Math.random() < 1 / 7) {
-						if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-							_entity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 40, 0, false, false));
-<<<<<<< HEAD
-					} else if (Math.random() < 0.14) {
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.CRAMMING), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
-					} else if (Math.random() < 0.14) {
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.LIGHTNING_BOLT), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
-=======
-					} else if (Math.random() < 1 / 7) {
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.CRAMMING)), Mth.nextInt(RandomSource.create(), 1, 10));
-					} else if (Math.random() < 1 / 7) {
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.LIGHTNING_BOLT)), Mth.nextInt(RandomSource.create(), 1, 10));
->>>>>>> parent of ece4629 (修正伤害原实体)
-						if (world instanceof ServerLevel _level) {
-							LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
-							entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y, z)));
-							entityToSpawn.setVisualOnly(true);
-							_level.addFreshEntity(entityToSpawn);
+					PrimogemcraftMod.queueServerWork(10, () -> {
+						if (Math.random() < 0.14) {
+							if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+								_entity.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 40, 0, false, false));
+						} else if (Math.random() < 0.14) {
+							entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.CRAMMING), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
+						} else if (Math.random() < 0.14) {
+							entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.LIGHTNING_BOLT), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
+							if (world instanceof ServerLevel _level) {
+								LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
+								entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(x, y, z)));
+								entityToSpawn.setVisualOnly(true);
+								_level.addFreshEntity(entityToSpawn);
+							}
+						} else if (Math.random() < 0.14) {
+							if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+								_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 40, 0, false, false));
+						} else if (Math.random() < 0.14) {
+							entity.setAirSupply(-20);
+							entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.IN_WALL), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
+						} else if (Math.random() < 0.14) {
+							entity.setSecondsOnFire(3);
+						} else {
+							entity.setTicksFrozen(60);
 						}
-<<<<<<< HEAD
-					} else if (Math.random() < 0.14) {
-						if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-							_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 40, 0, false, false));
-					} else if (Math.random() < 0.14) {
-						entity.setAirSupply(-20);
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.IN_WALL), sourceentity), Mth.nextInt(RandomSource.create(), 1, 10));
-					} else if (Math.random() < 0.14) {
-=======
-					} else if (Math.random() < 1 / 7) {
-						if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-							_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 40, 0, false, false));
-					} else if (Math.random() < 1 / 7) {
-						entity.setAirSupply(-20);
-						entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.IN_WALL)), Mth.nextInt(RandomSource.create(), 1, 10));
-					} else if (Math.random() < 1 / 7) {
->>>>>>> parent of ece4629 (修正伤害原实体)
-						entity.setSecondsOnFire(3);
-					} else {
-						entity.setTicksFrozen(60);
-					}
+					});
 				}
 			}
 		}
