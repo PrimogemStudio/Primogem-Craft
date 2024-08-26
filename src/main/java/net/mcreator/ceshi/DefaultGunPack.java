9e9efbf9aeac91f6929c87897b79e8fd3f4af1bf
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-08-26 16:46:42 +0800

    Add loot table inject

--- src/main/java/net/mcreator/ceshi/DefaultGunPack.java
+++ src/main/java/net/mcreator/ceshi/DefaultGunPack.java
@@ -1,29 +1,10 @@
-/**
- * The code of this mod element is always locked.
- * <p>
- * You can register new events in this class too.
- * <p>
- * If you want to make a plain independent class, create it using
- * Project Browser -> New... and make sure to make the class
- * outside net.mcreator.ceshi as this package is managed by MCreator.
- * <p>
- * If you change workspace package, modid or prefix, you will need
- * to manually adapt this file to these changes or remake it.
- * <p>
- * This class will be added in the mod root package.
- */
 package net.mcreator.ceshi;
 
 import com.tacz.guns.api.resource.ResourceManager;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 import static net.mcreator.ceshi.PrimogemcraftMod.MODID;
 
