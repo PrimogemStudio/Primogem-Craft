399b2a930055a19a581c3b2c69209d88467d352a
Pepper MR <zheshixbde@outlook.com>
2024-08-30 19:25:21 +0800

    删除无意义块

--- src/main/java/net/mcreator/ceshi/procedures/Ceshi_3Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Ceshi_3Procedure.java
@@ -30,16 +30,6 @@ public class Ceshi_3Procedure {
 						return _retval.get();
 					}
 				}.getItemStack(0, entity)).getItem()).toString())), false);
-			if (entity instanceof Player _player && !_player.level().isClientSide())
-				_player.displayClientMessage(Component.literal((ForgeRegistries.ITEMS.getKey((new Object() {
-					public ItemStack getItemStack(int sltid, Entity entity) {
-						AtomicReference<ItemStack> _retval = new AtomicReference<>(ItemStack.EMPTY);
-						entity.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(capability -> {
-							_retval.set(capability.getStackInSlot(sltid).copy());
-						});
-						return _retval.get();
-					}
-				}.getItemStack(0, entity)).getItem()).toString())), false);
 		}
 	}
 }
