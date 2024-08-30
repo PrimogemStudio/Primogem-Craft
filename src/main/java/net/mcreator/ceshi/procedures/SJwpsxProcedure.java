8c175f843be5b56aab345fe9a15c672efd4a4aa5
Pepper MR <zheshixbde@outlook.com>
2024-08-30 20:49:27 +0800

    修复事件拾取，创建测试地图

--- src/main/java/net/mcreator/ceshi/procedures/SJwpsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/SJwpsxProcedure.java
@@ -1,9 +1,9 @@
 package net.mcreator.ceshi.procedures;
 
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
+import net.neoforged.neoforge.common.util.TriState;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.bus.api.ICancellableEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.LevelAccessor;
@@ -49,66 +49,67 @@ public class SJwpsxProcedure {
 		if (entity == null)
 			return;
 		ItemStack a = ItemStack.EMPTY;
-		a = (itemstack.copy());
-		if (a.getItem() == BuiltInRegistries.ITEM.get(new ResourceLocation("primogemcraft:sh_jwupin"))) {
-			if (event instanceof ICancellableEvent _cancellable) {
-				_cancellable.setCanceled(true);
-			}
-			if (!(entity instanceof LivingEntity _livEnt4 && _livEnt4.hasEffect(PrimogemcraftModMobEffects.SHIJIANBUCHUFA))) {
-				if (entity.isShiftKeyDown()) {
-					if (a.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("PGC_fumo_shijian_00")) {
-						if (entity instanceof ServerPlayer _ent) {
-							BlockPos _bpos = BlockPos.containing(x, y, z);
-							_ent.openMenu(new MenuProvider() {
-								@Override
-								public Component getDisplayName() {
-									return Component.literal("SJGUIfumo00");
-								}
+		if (!world.isClientSide()) {
+			a = (itemstack.copy());
+			if (a.getItem() == BuiltInRegistries.ITEM.get(new ResourceLocation("primogemcraft:sh_jwupin"))) {
+				if (event instanceof ItemEntityPickupEvent.Pre _event)
+					_event.setCanPickup(TriState.FALSE);
+				if (!(entity instanceof LivingEntity _livEnt5 && _livEnt5.hasEffect(PrimogemcraftModMobEffects.SHIJIANBUCHUFA))) {
+					if (entity.isShiftKeyDown()) {
+						if (a.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("PGC_fumo_shijian_00")) {
+							if (entity instanceof ServerPlayer _ent) {
+								BlockPos _bpos = BlockPos.containing(x, y, z);
+								_ent.openMenu(new MenuProvider() {
+									@Override
+									public Component getDisplayName() {
+										return Component.literal("SJGUIfumo00");
+									}
 
-								@Override
-								public boolean shouldTriggerClientSideContainerClosingOnOpen() {
-									return false;
-								}
+									@Override
+									public boolean shouldTriggerClientSideContainerClosingOnOpen() {
+										return false;
+									}
 
-								@Override
-								public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
-									return new SJGUIfumo00Menu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
-								}
-							}, _bpos);
+									@Override
+									public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+										return new SJGUIfumo00Menu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
+									}
+								}, _bpos);
+							}
 						}
-					}
-					if (a.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("PGC_fumo_shijian_01")) {
-						if (entity instanceof ServerPlayer _ent) {
-							BlockPos _bpos = BlockPos.containing(x, y, z);
-							_ent.openMenu(new MenuProvider() {
-								@Override
-								public Component getDisplayName() {
-									return Component.literal("SJGUIfumo01");
-								}
+						if (a.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("PGC_fumo_shijian_01")) {
+							if (entity instanceof ServerPlayer _ent) {
+								BlockPos _bpos = BlockPos.containing(x, y, z);
+								_ent.openMenu(new MenuProvider() {
+									@Override
+									public Component getDisplayName() {
+										return Component.literal("SJGUIfumo01");
+									}
 
-								@Override
-								public boolean shouldTriggerClientSideContainerClosingOnOpen() {
-									return false;
-								}
+									@Override
+									public boolean shouldTriggerClientSideContainerClosingOnOpen() {
+										return false;
+									}
 
-								@Override
-								public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
-									return new SJGUIfumo01Menu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
-								}
-							}, _bpos);
+									@Override
+									public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+										return new SJGUIfumo01Menu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(_bpos));
+									}
+								}, _bpos);
+							}
 						}
-					}
-				} else if (!(entity instanceof ServerPlayer _plr10 && _plr10.level() instanceof ServerLevel
-						&& _plr10.getAdvancements().getOrStartProgress(_plr10.server.getAdvancements().get(new ResourceLocation("primogemcraft:jdshijian_0"))).isDone())) {
-					if (entity instanceof Player _player && !_player.level().isClientSide())
-						_player.displayClientMessage(Component.literal("\u00A75\u6F5C\u884C\u4EE5\u62FE\u53D6\u4E8B\u4EF6\uFF01"), false);
-					if (entity instanceof ServerPlayer _player) {
-						AdvancementHolder _adv = _player.server.getAdvancements().get(new ResourceLocation("primogemcraft:jdshijian_0"));
-						if (_adv != null) {
-							AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-							if (!_ap.isDone()) {
-								for (String criteria : _ap.getRemainingCriteria())
-									_player.getAdvancements().award(_adv, criteria);
+					} else if (!(entity instanceof ServerPlayer _plr11 && _plr11.level() instanceof ServerLevel
+							&& _plr11.getAdvancements().getOrStartProgress(_plr11.server.getAdvancements().get(new ResourceLocation("primogemcraft:jdshijian_0"))).isDone())) {
+						if (entity instanceof Player _player && !_player.level().isClientSide())
+							_player.displayClientMessage(Component.literal("\u00A75\u6F5C\u884C\u4EE5\u62FE\u53D6\u4E8B\u4EF6\uFF01"), false);
+						if (entity instanceof ServerPlayer _player) {
+							AdvancementHolder _adv = _player.server.getAdvancements().get(new ResourceLocation("primogemcraft:jdshijian_0"));
+							if (_adv != null) {
+								AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+								if (!_ap.isDone()) {
+									for (String criteria : _ap.getRemainingCriteria())
+										_player.getAdvancements().award(_adv, criteria);
+								}
 							}
 						}
 					}
