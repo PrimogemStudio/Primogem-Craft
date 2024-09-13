0dd083721691a88c3d003dcca7b58eb722e9b11f
Pepper MR <zheshixbde@outlook.com>
2024-09-14 02:42:26 +0800

    测不准瞎修复损坏奇物获取

--- src/main/java/net/mcreator/ceshi/procedures/CcebuzhunxiashuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/CcebuzhunxiashuxingProcedure.java
@@ -1,19 +1,20 @@
 package net.mcreator.ceshi.procedures;
 
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.items.ItemHandlerHelper;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
@@ -32,10 +33,10 @@ public class CcebuzhunxiashuxingProcedure {
 						_player.displayClientMessage(Component.literal("\u00A75\u7CFB\u7EDF\uFF1A\u300E\u5947\u7269\u300F\u00A7e\u6D4B\u4E0D\u51C6\u5323\u00A7c\u65E0\u6CD5\u5E72\u6D89"), false);
 				} else {
 					(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).shrink(1);
-					if (entity instanceof Player _player) {
-						ItemStack _setstack = new ItemStack(PrimogemcraftModItems.SHCBZX.get()).copy();
-						_setstack.setCount(1);
-						ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+					if (world instanceof ServerLevel _level) {
+						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.SHCBZX.get()));
+						entityToSpawn.setPickUpDelay(0);
+						_level.addFreshEntity(entityToSpawn);
 					}
 					if (world instanceof Level _level) {
 						if (!_level.isClientSide()) {
