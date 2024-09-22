cf160591bba9aa5310fc74ff181e3b787127cbfb
Pepper MR <zheshixbde@outlook.com>
2024-09-22 21:59:04 +0800

    万象骰逻辑

---
+++ src/main/java/net/mcreator/ceshi/procedures/HSQwwxwctsxProcedure.java
@@ -0,0 +1,92 @@
+package net.mcreator.ceshi.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.BlockPos;
+
+public class HSQwwxwctsxProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack item0, ItemStack itemstack, boolean bao_zang, boolean dan_ci, boolean tong_zhi, boolean xiao_hui, boolean yi_fumo, double dengji,
+			double zui_da, String itemx, String xianzhi_cishu) {
+		if (entity == null || itemx == null || xianzhi_cishu == null)
+			return;
+		if ((yi_fumo ? item0.isEnchanted() : item0.isEnchantable()) && !item0.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean(xianzhi_cishu)) {
+			item0.applyComponents((EnchantmentHelper.enchantItem(FeatureFlagSet.of(FeatureFlags.VANILLA), RandomSource.create(), (item0.copy()), (int) dengji, bao_zang)).getComponents());
+			if (dan_ci) {
+				{
+					final String _tagName = xianzhi_cishu;
+					final boolean _tagValue = true;
+					CustomData.update(DataComponents.CUSTOM_DATA, item0, tag -> tag.putBoolean(_tagName, _tagValue));
+				}
+			} else {
+				{
+					final String _tagName = xianzhi_cishu;
+					final double _tagValue = (item0.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble(xianzhi_cishu) + 1);
+					CustomData.update(DataComponents.CUSTOM_DATA, item0, tag -> tag.putDouble(_tagName, _tagValue));
+				}
+			}
+			if (xiao_hui) {
+				itemstack.shrink(1);
+			} else {
+				{
+					ItemStack _ist = itemstack;
+					_ist.hurtAndBreak(1, RandomSource.create(), null, () -> {
+						_ist.shrink(1);
+						_ist.setDamageValue(0);
+					});
+				}
+			}
+			if (xiao_hui) {
+				if (tong_zhi) {
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1);
+						} else {
+							_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1, false);
+						}
+					}
+					if (entity instanceof Player _player)
+						_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
+					if (entity instanceof Player _player && !_player.level().isClientSide())
+						_player.displayClientMessage(Component.literal((itemx + "\u00A7c\u5DF2\u6D88\u8017")), false);
+				}
+			} else if (itemstack.getDamageValue() == itemstack.getMaxDamage() - 1) {
+				if (tong_zhi) {
+					itemstack.shrink(1);
+					if (entity instanceof Player _player)
+						_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1);
+						} else {
+							_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1, false);
+						}
+					}
+					if (entity instanceof Player _player && !_player.level().isClientSide())
+						_player.displayClientMessage(Component.literal((itemx + "\u00A7c\u5DF2\u6D88\u8017")), false);
+				}
+			}
+		} else {
+			if (tong_zhi) {
+				if (entity instanceof Player _player && !_player.level().isClientSide())
+					_player.displayClientMessage(Component.literal(
+							(item0.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean(xianzhi_cishu) || item0.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble(xianzhi_cishu) >= zui_da
+									? "\u00A7c\u00A7l\u8BE5\u88C5\u5907\u5DF2\u65E0\u6CD5\u7EE7\u7EED\u4F7F\u7528\u8BE5\u5947\u7269"
+									: "\u00A75\u63D0\u4F9B\u7684\u7269\u54C1\u65E0\u6548")),
+							false);
+			}
+		}
+	}
+}
--- src/main/java/net/mcreator/ceshi/procedures/WawanxiangwuchangtoushuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/WawanxiangwuchangtoushuxingProcedure.java
@@ -1,55 +1,16 @@
 package net.mcreator.ceshi.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.flag.FeatureFlagSet;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.util.RandomSource;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.BlockPos;
 
 public class WawanxiangwuchangtoushuxingProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).isEnchanted()
-				&& !((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("wanxiangtou_xianzhi"))) {
-			(entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).applyComponents(
-					(EnchantmentHelper.enchantItem(FeatureFlagSet.of(FeatureFlags.VANILLA), RandomSource.create(), ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).copy()), 30, false)).getComponents());
-			{
-				final String _tagName = "wanxiangtou_xianzhi";
-				final boolean _tagValue = true;
-				CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY), tag -> tag.putBoolean(_tagName, _tagValue));
-			}
-			itemstack.shrink(1);
-			if (world instanceof Level _level) {
-				if (!_level.isClientSide()) {
-					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1);
-				} else {
-					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(new ResourceLocation("primogemcraft:qiwusunhuai066")), SoundSource.PLAYERS, 5, 1, false);
-				}
-			}
-			if (entity instanceof Player _player)
-				_player.getCooldowns().addCooldown(itemstack.getItem(), 20);
-			if (entity instanceof Player _player && !_player.level().isClientSide())
-				_player.displayClientMessage(Component.literal("\u00A75\u7CFB\u7EDF\uFF1A\u300E\u5947\u7269\u300F\u00A72\u4E07\u8C61\u65E0\u5E38\u9AB0\u00A7c\u5DF2\u6D88\u8017"), false);
-		} else {
-			if (entity instanceof Player _player && !_player.level().isClientSide())
-				_player.displayClientMessage(
-						Component.literal(((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getBoolean("wanxiangtou_xianzhi")
-								? "\u00A77\u8BE5\u88C5\u5907\u5DF2\u4F7F\u7528\u8FC7\u00A7c\u4E07\u8C61\u65E0\u5E38\u9AB0\uFF01"
-								: "\u00A75\u7CFB\u7EDF\uFF1A\u300E\u5947\u7269\u300F\u00A72\u4E07\u8C61\u65E0\u5E38\u9AB0\u00A7c\u4E00\u8138\u61F5\u54D4\u54E9")),
-						false);
-		}
+		HSQwwxwctsxProcedure.execute(world, x, y, z, entity, entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY, itemstack, false, true, true, true, true, 30, 1, itemstack.getDisplayName().getString(),
+				BuiltInRegistries.ITEM.getKey(itemstack.getItem()).toString() + "xianzhi");
 	}
 }
