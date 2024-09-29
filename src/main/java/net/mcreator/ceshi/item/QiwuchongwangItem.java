ebcae806d80cd763676d5a8bd173b4b0c122038d
Pepper MR <zheshixbde@outlook.com>
2024-09-29 20:13:04 +0800

    修改虫网描述，加强虫果

--- src/main/java/net/mcreator/ceshi/item/QiwuchongwangItem.java
+++ src/main/java/net/mcreator/ceshi/item/QiwuchongwangItem.java
@@ -21,14 +21,14 @@ public class QiwuchongwangItem extends Item {
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, context, list, flag);
-		list.add(Component.literal("\u00A76\u653B\u51FB\u751F\u7269\u65F6\uFF0C\u8D4B\u4E88\u00A7d\u00A7l\u5BC4\u751F\u00A7r\u00A76\u72B6\u6001"));
-		list.add(Component.literal("\u00A75\u5904\u4E8E\u00A7d\u00A7l\u5BC4\u751F\u00A7r\u00A75\u72B6\u6001\u65F6\uFF0C\u57FA\u7840\u653B\u51FB\u529B"));
-		list.add(Component.literal("\u00A75\u989D\u5916\u589E\u52A050%"));
-		list.add(Component.literal("\u00A7c\u00A7l\u6740\u6B7B\u751F\u7269\u540E\u5BF9\u81EA\u8EAB\u9020\u6210\u4E00\u6B21\u5F53"));
-		list.add(Component.literal("\u00A7c\u00A7l\u524D\u751F\u547D\u503C20%\u7684\u771F\u5B9E\u4F24\u5BB3"));
+		list.add(Component.literal("\u00A7e\u00A7l\u653B\u51FB\u751F\u7269\u65F6\uFF0C\u83B7\u5F97\u00A7d\u00A7l\u5BC4\u751F\u00A7r\u00A7e\u00A7l\u72B6\u6001"));
+		list.add(Component.literal("\u00A75\u5904\u4E8E\u00A7d\u00A7l\u5BC4\u751F\u00A7r\u00A75\u72B6\u6001\u65F6:"));
+		list.add(Component.literal("\u00A76 \u00A7l\u57FA\u7840\u653B\u51FB\u529B\u989D\u5916\u589E\u52A050%"));
+		list.add(Component.literal("\u00A76 \u00A7l\u6740\u6B7B\u751F\u7269\u540E\u5BF9\u81EA\u8EAB\u9020\u6210\u4E00\u6B21\u5F53"));
+		list.add(Component.literal("\u00A76 \u00A7l\u524D\u751F\u547D\u503C20%\u7684\u771F\u5B9E\u4F24\u5BB3"));
 		list.add(Component.literal("\u00A7"));
 		list.add(Component.literal("\u00A79\u5BF9\u4E8E[\u5BC4\u751F]\u6548\u679C\u6BCF\u7EA7\uFF1A"));
 		list.add(Component.literal("\u00A79 +50%\u989D\u5916\u57FA\u7840\u653B\u51FB\u529B"));
-		list.add(Component.literal("\u00A79 +20%\u57FA\u4E8E\u751F\u547D\u503C\u53D7\u5230\u7684\u4F24\u5BB3"));
+		list.add(Component.literal("\u00A7c -20%\u5F53\u524D\u751F\u547D\u503C"));
 	}
 }
--- src/main/java/net/mcreator/ceshi/procedures/Yiyijieguoshi_shuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Yiyijieguoshi_shuxingProcedure.java
@@ -30,7 +30,7 @@ public class Yiyijieguoshi_shuxingProcedure {
 					final Vec3 _center = new Vec3(x, y, z);
 					List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(16 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 					for (Entity entityiterator : _entfound) {
-						if (!(entityiterator == entity) && (entityiterator instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) <= (entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 2) {
+						if (!(entityiterator == entity) && (entityiterator instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) <= (entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 10) {
 							if (!(entityiterator instanceof LivingEntity _livEnt15 && _livEnt15.hasEffect(PrimogemcraftModMobEffects.JISHENG))) {
 								if (entityiterator instanceof LivingEntity _entity && !_entity.level().isClientSide())
 									_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.JISHENG, 400, 4));
