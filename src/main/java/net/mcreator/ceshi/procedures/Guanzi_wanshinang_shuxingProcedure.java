2c6ed8e773c328916edc47772fadc350665876de
Pepper MR <zheshixbde@outlook.com>
2024-10-20 17:35:48 +0800

    附魔开罐器

--- src/main/java/net/mcreator/ceshi/procedures/Guanzi_wanshinang_shuxingProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Guanzi_wanshinang_shuxingProcedure.java
@@ -1,8 +1,19 @@
 package net.mcreator.ceshi.procedures;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 import net.mcreator.ceshi.init.PrimogemcraftModBlocks;
 import net.mcreator.ceshi.PrimogemcraftMod;
@@ -21,8 +32,28 @@ public class Guanzi_wanshinang_shuxingProcedure {
 			entity.getPersistentData().putDouble("guan_zi_z", z);
 			if (blockstate.getBlock() == PrimogemcraftModBlocks.XIAOGUANZI.get()) {
 				entity.getPersistentData().putBoolean("xiao_guan_zi", true);
+				if (Math.random() < (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
+						.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("primogemcraft:fmkaiguanqi")))) * 0.1) {
+					if (world instanceof ServerLevel _level)
+						_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+								"loot spawn ~ ~ ~ loot primogemcraft:blocks/xiaoguanzi");
+					if (world instanceof ServerLevel _level) {
+						(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).hurtAndBreak(1, _level, null, _stkprov -> {
+						});
+					}
+				}
 			} else {
 				entity.getPersistentData().putBoolean("xiao_guan_zi", false);
+				if (Math.random() < (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
+						.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("primogemcraft:fmkaiguanqi")))) * 0.1) {
+					if (world instanceof ServerLevel _level)
+						_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+								"loot spawn ~ ~ ~ loot primogemcraft:blocks/daguanzi");
+					if (world instanceof ServerLevel _level) {
+						(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).hurtAndBreak(1, _level, null, _stkprov -> {
+						});
+					}
+				}
 			}
 		}
 	}
