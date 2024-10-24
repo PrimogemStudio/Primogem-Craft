0aad8d6440b29e2f1416e62964f2e86924b3c3bf
Pepper MR <zheshixbde@outlook.com>
2024-10-25 00:56:16 +0800

    补全

--- src/main/java/net/mcreator/ceshi/item/QwjlbxyqItem.java
+++ src/main/java/net/mcreator/ceshi/item/QwjlbxyqItem.java
@@ -25,10 +25,12 @@ public class QwjlbxyqItem extends Item {
 	@OnlyIn(Dist.CLIENT)
 	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, context, list, flag);
-		list.add(Component.literal("\u00A77\u653B\u51FB\u751F\u7269\u65F6\uFF0C\u5C06\u81EA\u8EAB10%\u6700\u5927\u751F\u547D\u503C"));
-		list.add(Component.literal("\u00A77\u7B49\u7EA7\u7684 \u751F\u547D\u5782\u5371 \u6548\u679C\u8D4B\u4E88\u751F\u7269"));
-		list.add(Component.literal("\u00A7c\u00A7l\u6BCF\u7EA7\u6548\u679C\u51CF\u5C11\u51762\u70B9\u751F\u547D\u503C\u4E0A\u9650"));
+		list.add(Component.literal("\u00A77\u653B\u51FB\u751F\u7269\u65F6\uFF0C\u5C06\u51CF\u5C11\u5176\u7684\u6700\u5927\u751F\u547D"));
+		list.add(Component.literal("\u00A77\u503C\uFF0C\u51CF\u5C11\u91CF\u4E3A\u5F53\u524D\u4F7F\u7528\u8005\u6700\u5927\u751F\u547D"));
+		list.add(Component.literal("\u00A77\u503C\u76845%"));
 		list.add(Component.literal("\u00A77\u00A7l \u6548\u679C\u6301\u7EED10s\uFF0C\u89E6\u53D1\u540E\u51B7\u537415s"));
+		list.add(Component.literal("\u00A7e\u6BCF\u6B21\u89E6\u53D1\u540E20%\u6389\u843D1\u4E2A\u5B87\u5B99\u788E\u7247"));
+		list.add(Component.literal("\u00A7a\u00A7l\u643A\u5E26\u540E\u9ED1\u5854\u5546\u5E97\u5947\u7269\u4EF7\u683C\u51CF\u5C1120%"));
 	}
 
 	@Override
--- src/main/java/net/mcreator/ceshi/procedures/QwjlbxypzsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/QwjlbxypzsxProcedure.java
@@ -5,11 +5,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerLevel;
 
 import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
 import net.mcreator.ceshi.init.PrimogemcraftModItems;
@@ -21,23 +24,32 @@ public class QwjlbxypzsxProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingIncomingDamageEvent event) {
 		if (event.getEntity() != null) {
-			execute(event, event.getEntity(), event.getSource().getEntity());
+			execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity(), event.getSource().getEntity());
 		}
 	}
 
-	public static void execute(Entity entity, Entity sourceentity) {
-		execute(null, entity, sourceentity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		execute(null, world, x, y, z, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, Entity sourceentity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
-		if ((sourceentity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.QWJLBXYQ.get())) : false)
-				&& !(sourceentity instanceof Player _plrCldCheck1 && _plrCldCheck1.getCooldowns().isOnCooldown(PrimogemcraftModItems.QWJLBXYQ.get()))) {
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.X_GSMCW, 200, (int) ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 0.1 - 1)));
-			if (sourceentity instanceof Player _player)
-				_player.getCooldowns().addCooldown(PrimogemcraftModItems.QWJLBXYQ.get(), 300);
+		if (!world.isClientSide()) {
+			if ((sourceentity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.QWJLBXYQ.get())) : false)
+					&& !(sourceentity instanceof Player _plrCldCheck2 && _plrCldCheck2.getCooldowns().isOnCooldown(PrimogemcraftModItems.QWJLBXYQ.get()))) {
+				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+					_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.X_GSMCW, 200, (int) ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 0.1 - 1)));
+				if (Math.random() < 0.2) {
+					if (world instanceof ServerLevel _level) {
+						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get()));
+						entityToSpawn.setPickUpDelay(10);
+						_level.addFreshEntity(entityToSpawn);
+					}
+				}
+				if (sourceentity instanceof Player _player)
+					_player.getCooldowns().addCooldown(PrimogemcraftModItems.QWJLBXYQ.get(), 300);
+			}
 		}
 	}
 }
