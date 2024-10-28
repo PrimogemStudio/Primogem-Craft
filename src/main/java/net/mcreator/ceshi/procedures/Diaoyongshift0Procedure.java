0712b3d3ca4a03c350382906e3e1601176d684ae
Pepper MR <zheshixbde@outlook.com>
2024-10-28 18:56:56 +0800

    按住shift

---
+++ src/main/java/net/mcreator/ceshi/procedures/Diaoyongshift0Procedure.java
@@ -0,0 +1,9 @@
+package net.mcreator.ceshi.procedures;
+
+import net.minecraft.client.gui.screens.Screen;
+
+public class Diaoyongshift0Procedure {
+	public static boolean execute() {
+		return Screen.hasShiftDown();
+	}
+}
---
+++ src/main/java/net/mcreator/ceshi/procedures/Diaoyongshift1Procedure.java
@@ -0,0 +1,7 @@
+package net.mcreator.ceshi.procedures;
+
+public class Diaoyongshift1Procedure {
+	public static String execute() {
+		return "\u00A77\u00A7o\u6309\u4F4F[Shift]\u67E5\u770B\u8BE6\u60C5";
+	}
+}
