813657db1afb391a053524a58c44f95760aedd54
Pepper MR <zheshixbde@outlook.com>
2024-11-09 02:27:34 +0800

    草套优化

--- src/main/java/net/mcreator/ceshi/procedures/SzkjsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/SzkjsxProcedure.java
@@ -3,14 +3,11 @@ package net.mcreator.ceshi.procedures;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.component.DataComponents;
 
 import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
@@ -22,27 +19,7 @@ public class SzkjsxProcedure {
 			return;
 		double a = 0;
 		if (!world.isClientSide()) {
-			if (itemstack.is(ItemTags.create(ResourceLocation.parse("primogemcraft:yuansutaotie")))) {
-				{
-					final String _tagName = "shengzheng_tao_zhi";
-					final double _tagValue = ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQCAO.get())) : false) ? 1 : 0.5);
-					CustomData.update(DataComponents.CUSTOM_DATA, itemstack, tag -> tag.putDouble(_tagName, _tagValue));
-				}
-			}
-			if (itemstack.is(ItemTags.create(ResourceLocation.parse("primogemcraft:yuansutaozuanshi")))) {
-				{
-					final String _tagName = "shengzheng_tao_zhi";
-					final double _tagValue = ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQCAO.get())) : false) ? 1.5 : 1);
-					CustomData.update(DataComponents.CUSTOM_DATA, itemstack, tag -> tag.putDouble(_tagName, _tagValue));
-				}
-			}
-			if (itemstack.is(ItemTags.create(ResourceLocation.parse("primogemcraft:yuansutaoxiajiehejin")))) {
-				{
-					final String _tagName = "shengzheng_tao_zhi";
-					final double _tagValue = ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQCAO.get())) : false) ? 2.5 : 2);
-					CustomData.update(DataComponents.CUSTOM_DATA, itemstack, tag -> tag.putDouble(_tagName, _tagValue));
-				}
-			}
+			HuoqihanshuProcedure.execute(entity, new ItemStack(PrimogemcraftModItems.HQCAO.get()), itemstack, "shengzheng_tao_zhi");
 			a = a + (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("shengzheng_tao_zhi")
 					+ (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("shengzheng_tao_zhi")
 					+ (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("shengzheng_tao_zhi")
@@ -53,7 +30,7 @@ public class SzkjsxProcedure {
 						_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 10, 0, true, false));
 				}
 			}
-			if (!(entity instanceof LivingEntity _livEnt25 && _livEnt25.hasEffect(MobEffects.HEALTH_BOOST)) && !(entity instanceof LivingEntity _livEnt26 && _livEnt26.hasEffect(PrimogemcraftModMobEffects.SZTSXCWDP))) {
+			if (!(entity instanceof LivingEntity _livEnt10 && _livEnt10.hasEffect(MobEffects.HEALTH_BOOST)) && !(entity instanceof LivingEntity _livEnt11 && _livEnt11.hasEffect(PrimogemcraftModMobEffects.SZTSXCWDP))) {
 				entity.getPersistentData().putDouble("yuanbenshengminzhi", (entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1));
 				if (a >= 4) {
 					if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
