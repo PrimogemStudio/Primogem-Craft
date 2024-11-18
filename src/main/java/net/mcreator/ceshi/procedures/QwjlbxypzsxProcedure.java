ff2672c605f1e56d557bebb86c90b63595b32df4
Pepper MR <zheshixbde@outlook.com>
2024-11-19 03:52:33 +0800

    融合奇物修改

--- src/main/java/net/mcreator/ceshi/procedures/QwjlbxypzsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/QwjlbxypzsxProcedure.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -39,7 +40,8 @@ public class QwjlbxypzsxProcedure {
 			if ((sourceentity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.QWJLBXYQ.get())) : false)
 					&& !(sourceentity instanceof Player _plrCldCheck2 && _plrCldCheck2.getCooldowns().isOnCooldown(PrimogemcraftModItems.QWJLBXYQ.get()))) {
 				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-					_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.X_GSMCW, 200, (int) ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1) * 0.1 - 1)));
+					_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.X_GSMCW, 200,
+							(int) ((sourceentity instanceof LivingEntity _livingEntity3 && _livingEntity3.getAttributes().hasAttribute(Attributes.MAX_HEALTH) ? _livingEntity3.getAttribute(Attributes.MAX_HEALTH).getValue() : 0) * 0.1 - 1)));
 				if (Math.random() < 0.2) {
 					if (world instanceof ServerLevel _level) {
 						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get()));
