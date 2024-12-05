55d833e169b5b99a77893dda4d0d5b070af2d686
Pepper MR <zheshixbde@outlook.com>
2024-12-05 09:33:28 +0800

    花火优化

--- src/main/java/net/mcreator/ceshi/procedures/Hh_sx_0Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Hh_sx_0Procedure.java
@@ -33,6 +33,8 @@ import java.util.Comparator;
 
 public class Hh_sx_0Procedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
+		double n1 = 0;
+		double n2 = 0;
 		if (!world.isClientSide()) {
 			if (!world.isClientSide()) {
 				BlockPos _bp = BlockPos.containing(x, y, z);
@@ -66,14 +68,23 @@ public class Hh_sx_0Procedure {
 				if (world instanceof Level _level)
 					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
 			}
-			if (new Object() {
+			n1 = new Object() {
 				public double getValue(LevelAccessor world, BlockPos pos, String tag) {
 					BlockEntity blockEntity = world.getBlockEntity(pos);
 					if (blockEntity != null)
 						return blockEntity.getPersistentData().getDouble(tag);
 					return -1;
 				}
-			}.getValue(world, BlockPos.containing(x, y, z), "daojishi") >= 600) {
+			}.getValue(world, BlockPos.containing(x, y, z), "daojishi");
+			n2 = new Object() {
+				public double getValue(LevelAccessor world, BlockPos pos, String tag) {
+					BlockEntity blockEntity = world.getBlockEntity(pos);
+					if (blockEntity != null)
+						return blockEntity.getPersistentData().getDouble(tag);
+					return -1;
+				}
+			}.getValue(world, BlockPos.containing(x, y, z), "daojishi2");
+			if (n1 >= 600) {
 				if (world instanceof Level _level) {
 					if (!_level.isClientSide()) {
 						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("primogemcraft:hh_boo")), SoundSource.BLOCKS, 1, 1);
@@ -155,220 +166,12 @@ public class Hh_sx_0Procedure {
 					}
 				}
 			} else {
-				if (new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi") < 400 && new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi2") >= 16) {
-					{
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockState _bs = PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState();
-						BlockState _bso = world.getBlockState(_bp);
-						for (Property<?> _propertyOld : _bso.getProperties()) {
-							Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-							if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-								try {
-									_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-								} catch (Exception e) {
-								}
-						}
-						BlockEntity _be = world.getBlockEntity(_bp);
-						CompoundTag _bnbt = null;
-						if (_be != null) {
-							_bnbt = _be.saveWithFullMetadata(world.registryAccess());
-							_be.setRemoved();
-						}
-						world.setBlock(_bp, _bs, 3);
-						if (_bnbt != null) {
-							_be = world.getBlockEntity(_bp);
-							if (_be != null) {
-								try {
-									_be.loadWithComponents(_bnbt, world.registryAccess());
-								} catch (Exception ignored) {
-								}
-							}
-						}
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi", (new Object() {
-								public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-									BlockEntity blockEntity = world.getBlockEntity(pos);
-									if (blockEntity != null)
-										return blockEntity.getPersistentData().getDouble(tag);
-									return -1;
-								}
-							}.getValue(world, BlockPos.containing(x, y, z), "daojishi")));
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi2", 0);
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
-				} else if (new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi") > 400 && new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi") < 560 && new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi2") >= 8) {
-					{
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockState _bs = PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState();
-						BlockState _bso = world.getBlockState(_bp);
-						for (Property<?> _propertyOld : _bso.getProperties()) {
-							Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-							if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-								try {
-									_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-								} catch (Exception e) {
-								}
-						}
-						BlockEntity _be = world.getBlockEntity(_bp);
-						CompoundTag _bnbt = null;
-						if (_be != null) {
-							_bnbt = _be.saveWithFullMetadata(world.registryAccess());
-							_be.setRemoved();
-						}
-						world.setBlock(_bp, _bs, 3);
-						if (_bnbt != null) {
-							_be = world.getBlockEntity(_bp);
-							if (_be != null) {
-								try {
-									_be.loadWithComponents(_bnbt, world.registryAccess());
-								} catch (Exception ignored) {
-								}
-							}
-						}
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi", (new Object() {
-								public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-									BlockEntity blockEntity = world.getBlockEntity(pos);
-									if (blockEntity != null)
-										return blockEntity.getPersistentData().getDouble(tag);
-									return -1;
-								}
-							}.getValue(world, BlockPos.containing(x, y, z), "daojishi")));
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi2", 0);
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
-				} else if (new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi") > 560 && new Object() {
-					public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-						BlockEntity blockEntity = world.getBlockEntity(pos);
-						if (blockEntity != null)
-							return blockEntity.getPersistentData().getDouble(tag);
-						return -1;
-					}
-				}.getValue(world, BlockPos.containing(x, y, z), "daojishi2") >= 4) {
-					{
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockState _bs = PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState();
-						BlockState _bso = world.getBlockState(_bp);
-						for (Property<?> _propertyOld : _bso.getProperties()) {
-							Property _propertyNew = _bs.getBlock().getStateDefinition().getProperty(_propertyOld.getName());
-							if (_propertyNew != null && _bs.getValue(_propertyNew) != null)
-								try {
-									_bs = _bs.setValue(_propertyNew, _bso.getValue(_propertyOld));
-								} catch (Exception e) {
-								}
-						}
-						BlockEntity _be = world.getBlockEntity(_bp);
-						CompoundTag _bnbt = null;
-						if (_be != null) {
-							_bnbt = _be.saveWithFullMetadata(world.registryAccess());
-							_be.setRemoved();
-						}
-						world.setBlock(_bp, _bs, 3);
-						if (_bnbt != null) {
-							_be = world.getBlockEntity(_bp);
-							if (_be != null) {
-								try {
-									_be.loadWithComponents(_bnbt, world.registryAccess());
-								} catch (Exception ignored) {
-								}
-							}
-						}
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi", (new Object() {
-								public double getValue(LevelAccessor world, BlockPos pos, String tag) {
-									BlockEntity blockEntity = world.getBlockEntity(pos);
-									if (blockEntity != null)
-										return blockEntity.getPersistentData().getDouble(tag);
-									return -1;
-								}
-							}.getValue(world, BlockPos.containing(x, y, z), "daojishi")));
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
-					if (!world.isClientSide()) {
-						BlockPos _bp = BlockPos.containing(x, y, z);
-						BlockEntity _blockEntity = world.getBlockEntity(_bp);
-						BlockState _bs = world.getBlockState(_bp);
-						if (_blockEntity != null)
-							_blockEntity.getPersistentData().putDouble("daojishi2", 0);
-						if (world instanceof Level _level)
-							_level.sendBlockUpdated(_bp, _bs, _bs, 3);
-					}
+				if (n1 < 400 && n2 >= 16) {
+					HuahuohsProcedure.execute(world, x, y, z, PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState(), n1);
+				} else if (n1 > 400 && n1 < 560 && n2 >= 8) {
+					HuahuohsProcedure.execute(world, x, y, z, PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState(), n1);
+				} else if (n1 > 560 && n2 >= 4) {
+					HuahuohsProcedure.execute(world, x, y, z, PrimogemcraftModBlocks.HHZD_1.get().defaultBlockState(), n1);
 				}
 			}
 		}
