3a76cb56eb683c85c7817588c9895d99835524fe
Pepper MR <zheshixbde@outlook.com>
2025-02-11 19:32:26 +0800

    魔阴身加强

--- src/main/java/net/mcreator/ceshi/procedures/Fengrao_shuxing_4Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Fengrao_shuxing_4Procedure.java
@@ -6,13 +6,17 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
 
+import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
+
 public class Fengrao_shuxing_4Procedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
 		if (!(entity instanceof Player)) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 1, false, false));
+				_entity.addEffect(new MobEffectInstance(PrimogemcraftModMobEffects.GONGJITISHENG, 200, 99, false, false));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 0, false, false));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 2, false, false));
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
