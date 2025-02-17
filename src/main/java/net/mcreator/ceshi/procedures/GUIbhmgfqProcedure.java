e9c1343cd8529ec170f181e39be545c25d8ce12b
Pepper MR <zheshixbde@outlook.com>
2025-02-18 05:20:43 +0800

    修复捕获明光星辉缺失问题

--- src/main/java/net/mcreator/ceshi/procedures/GUIbhmgfqProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/GUIbhmgfqProcedure.java
@@ -4,11 +4,17 @@ import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 
+import net.mcreator.ceshi.PrimogemcraftMod;
+
 public class GUIbhmgfqProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		GUIqingchu012Procedure.execute(entity);
+		entity.getPersistentData().putBoolean("truea", true);
+		PrimogemcraftMod.queueServerWork(5, () -> {
+			entity.getPersistentData().putBoolean("truea", false);
+		});
 		if (entity instanceof Player _player)
 			_player.closeContainer();
 		QycjzlpProcedure.execute(world, x, y, z);
--- src/main/java/net/mcreator/ceshi/procedures/GUIbhmgsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/GUIbhmgsxProcedure.java
@@ -1,6 +1,7 @@
 package net.mcreator.ceshi.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.entity.player.Player;
@@ -8,7 +9,13 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.ceshi.init.PrimogemcraftModItems;
 
 import java.util.function.Supplier;
 import java.util.Map;
@@ -18,7 +25,10 @@ public class GUIbhmgsxProcedure {
 		if (entity == null)
 			return;
 		double n1 = 0;
+		ItemStack i1 = ItemStack.EMPTY;
 		if (!world.isClientSide()) {
+			i1 = new ItemStack(PrimogemcraftModItems.XINGHUI.get());
+			i1.setCount(Mth.nextInt(RandomSource.create(), 10, 20));
 			if (!GUIbhmgsxhs1Procedure.execute(entity)) {
 				if (world instanceof ServerLevel _level) {
 					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z,
@@ -27,8 +37,28 @@ public class GUIbhmgsxProcedure {
 					entityToSpawn.setUnlimitedLifetime();
 					_level.addFreshEntity(entityToSpawn);
 				}
+				if (world instanceof ServerLevel _level) {
+					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, i1);
+					entityToSpawn.setPickUpDelay(0);
+					entityToSpawn.setUnlimitedLifetime();
+					_level.addFreshEntity(entityToSpawn);
+				}
 				n1 = 0;
 				GUIqingchu012Procedure.execute(entity);
+			} else if (!entity.getPersistentData().getBoolean("truea")) {
+				if (world instanceof ServerLevel _level) {
+					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, i1);
+					entityToSpawn.setPickUpDelay(0);
+					entityToSpawn.setUnlimitedLifetime();
+					_level.addFreshEntity(entityToSpawn);
+				}
+			}
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("primogemcraft:dashengchujin")), SoundSource.NEUTRAL, 2, 1);
+				} else {
+					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("primogemcraft:dashengchujin")), SoundSource.NEUTRAL, 2, 1, false);
+				}
 			}
 		}
 	}
