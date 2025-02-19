7c2fc394e24fe6d1de2882e7f0b372dc77ab89b3
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-02-19 18:43:58 +0800

    Nothing

--- src/main/java/net/mcreator/ceshi/procedures/ExampleTestProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/ExampleTestProcedure.java
@@ -1,11 +1,6 @@
 package net.mcreator.ceshi.procedures;
 
-import net.minecraft.world.item.component.FireworkExplosion;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.client.particle.FireworkParticles;
-import net.minecraft.client.Minecraft;
-
-import java.util.List;
 
 public class ExampleTestProcedure {
 	public static void execute() {
@@ -13,13 +8,16 @@ public class ExampleTestProcedure {
 		double x = 0;
 		double y = 0;
 		double z = 0;
-		new Object() {
-			void run(double x, double y, double z) {
-				var colors = it.unimi.dsi.fastutil.ints.IntList.of(0xFF9FE0, 0xAEEDFF);
-				var fadeColors = it.unimi.dsi.fastutil.ints.IntList.of(0xF4C6FF);
-				var pt = new FireworkParticles.Starter(Minecraft.getInstance().level, x, y, z, 0, 0, 0, Minecraft.getInstance().particleEngine, List.of(FireworkExplosion.DEFAULT));
-				pt.createParticleBall(0.5, 3, colors, fadeColors, true, false);
-			}
-		}.run(x, y, z);
+		double R_radius = 0;
+		double D_delta_theta = 0;
+		double C_cycle = 0;
+		double T_theta = 0;
+		double N_number = 0;
+		N_number = 42;
+		R_radius = 3;
+		D_delta_theta = (2 * Math.PI) / N_number;
+		for (int index0 = 0; index0 < (int) N_number; index0++) {
+			T_theta = index0 * D_delta_theta;
+		}
 	}
 }
