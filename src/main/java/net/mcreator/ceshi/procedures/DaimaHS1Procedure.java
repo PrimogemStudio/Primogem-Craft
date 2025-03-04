8aaa785da573fdc9f03773dadb953d7cbf628708
Pepper MR <zheshixbde@outlook.com>
2025-03-05 05:32:29 +0800

    补全

--- src/main/java/net/mcreator/ceshi/procedures/DaimaHS1Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/DaimaHS1Procedure.java
@@ -2,12 +2,6 @@ package net.mcreator.ceshi.procedures;
 
 public class DaimaHS1Procedure {
 	public static double execute(boolean xiao_guo, double zhi_1) {
-		double p = 0;
-		double n1 = 0;
-		double n2 = 0;
-		double ns = 0;
-		double nx1 = 0;
-		double nx2 = 0;
 		if (xiao_guo) {
 			return zhi_1 + 400;
 		}
