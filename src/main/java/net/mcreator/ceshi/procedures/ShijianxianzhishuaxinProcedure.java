48f6d75a5eb67f3a2c17ce8cfb0ac1db1bd6bfbb
Pepper MR <zheshixbde@outlook.com>
2025-03-05 10:20:22 +0800

    随机事件刷新条件
    改为起床时刷新

--- src/main/java/net/mcreator/ceshi/procedures/ShijianxianzhishuaxinProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/ShijianxianzhishuaxinProcedure.java
@@ -1,6 +1,6 @@
 package net.mcreator.ceshi.procedures;
 
-import net.neoforged.neoforge.event.tick.LevelTickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerWakeUpEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
@@ -14,8 +14,8 @@ import javax.annotation.Nullable;
 @EventBusSubscriber
 public class ShijianxianzhishuaxinProcedure {
 	@SubscribeEvent
-	public static void onWorldTick(LevelTickEvent.Post event) {
-		execute(event, event.getLevel());
+	public static void onEntityEndSleep(PlayerWakeUpEvent event) {
+		execute(event, event.getEntity().level());
 	}
 
 	public static void execute(LevelAccessor world) {
@@ -24,12 +24,8 @@ public class ShijianxianzhishuaxinProcedure {
 
 	private static void execute(@Nullable Event event, LevelAccessor world) {
 		if (!world.isClientSide()) {
-			if (Math.floor(world.dayTime() / 24000) != PrimogemcraftModVariables.MapVariables.get(world).jishiqi_meiri) {
-				PrimogemcraftModVariables.MapVariables.get(world).jishiqi_meiri = Math.floor(world.dayTime() / 24000);
-				PrimogemcraftModVariables.MapVariables.get(world).syncData(world);
-				PrimogemcraftModVariables.MapVariables.get(world).shijian_xianzhi = 0;
-				PrimogemcraftModVariables.MapVariables.get(world).syncData(world);
-			}
+			PrimogemcraftModVariables.MapVariables.get(world).shijian_xianzhi = 0;
+			PrimogemcraftModVariables.MapVariables.get(world).syncData(world);
 		}
 	}
 }
