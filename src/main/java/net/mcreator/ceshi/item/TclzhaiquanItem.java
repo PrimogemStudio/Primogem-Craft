e3901e2be0db7dab27b3d8c5e7e8f1f86acfc691
Pepper MR <zheshixbde@outlook.com>
2025-03-25 04:16:03 +0800

    添加一堆描述

--- src/main/java/net/mcreator/ceshi/item/TclzhaiquanItem.java
+++ src/main/java/net/mcreator/ceshi/item/TclzhaiquanItem.java
@@ -1,27 +1,11 @@
 
 package net.mcreator.ceshi.item;
 
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.api.distmarker.Dist;
-
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
 
 public class TclzhaiquanItem extends Item {
 	public TclzhaiquanItem() {
 		super(new Item.Properties().stacksTo(64).fireResistant().rarity(Rarity.COMMON));
-	}
-
-	@Override
-	@OnlyIn(Dist.CLIENT)
-	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
-		super.appendHoverText(itemstack, context, list, flag);
-		list.add(Component.translatable("item.primogemcraft.tclzhaiquan.description_0"));
-		list.add(Component.translatable("item.primogemcraft.tclzhaiquan.description_1"));
 	}
 }
