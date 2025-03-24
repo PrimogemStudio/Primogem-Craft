73828343507919486be6161a5d089aa13d743fc8
Pepper MR <zheshixbde@outlook.com>
2025-03-25 02:56:10 +0800

    兼容视觉效果

--- src/main/java/net/mcreator/ceshi/GenshinCraftLinkage.java
+++ src/main/java/net/mcreator/ceshi/GenshinCraftLinkage.java
@@ -5,7 +5,22 @@ import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
 
 public class GenshinCraftLinkage {
     static {
-        EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.JISHENG, 1);
-        EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.GONGJITISHENG, 1);
+        EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.JISHENG, 255);
+        EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.GONGJITISHENG, 10);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.X_GSMCW, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.ZHANDOUZHUANGTAI, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.FENGRAO, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.RYKJXG, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.JDSBCF_0XG, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.JDSBCF_1XG, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.JDSBCF_2XG, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.QWYMGS, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.LINZHONG, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.GUOQU, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.HSLGGZFUZHIPIN, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.FZGGZXG_0, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.FZGGZXG_1, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.HEISENLINGGZ, 255);
+		EffectRender.registerRenderEffect(PrimogemcraftModMobEffects.YYDGGZXG, 255);
     }
 }
