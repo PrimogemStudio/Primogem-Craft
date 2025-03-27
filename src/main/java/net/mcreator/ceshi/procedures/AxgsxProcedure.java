6d94893bc21ae3920896cfc7b39e26a0a92998fd
Pepper MR <zheshixbde@outlook.com>
2025-03-27 22:04:52 +0800

    调整远古代码
    冰稿

--- src/main/java/net/mcreator/ceshi/procedures/AxgsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/AxgsxProcedure.java
@@ -19,16 +19,18 @@ public class AxgsxProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate, Entity entity) {
 		if (entity == null)
 			return;
-		if (blockstate.is(BlockTags.create(ResourceLocation.parse("c:yuansujingtikuai")))) {
-			if (Math.random() < ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 0.15 : 0.1)) {
-				world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-				if (world instanceof ServerLevel _level) {
-					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get()));
-					entityToSpawn.setPickUpDelay(10);
-					_level.addFreshEntity(entityToSpawn);
+		if (!world.isClientSide()) {
+			if (blockstate.is(BlockTags.create(ResourceLocation.parse("c:yuansujingtikuai")))) {
+				if (Math.random() < ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 0.15 : 0.1)) {
+					world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+					if (world instanceof ServerLevel _level) {
+						ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get()));
+						entityToSpawn.setPickUpDelay(10);
+						_level.addFreshEntity(entityToSpawn);
+					}
+				} else if (Math.random() < 0.1) {
+					world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
 				}
-			} else if (Math.random() < 0.1) {
-				world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
 			}
 		}
 	}
--- src/main/java/net/mcreator/ceshi/procedures/YxaxgsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/YxaxgsxProcedure.java
@@ -8,6 +8,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -20,35 +22,26 @@ public class YxaxgsxProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, BlockState blockstate, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if (blockstate.getBlock() == PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get()) {
-			if (entity instanceof Player _player)
-				_player.getCooldowns().addCooldown(itemstack.getItem(), (int) ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 10 : 20));
-			if (Math.random() < ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 1 : 0.95)) {
-				if (world instanceof Level _level) {
-					if (!_level.isClientSide()) {
-						_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1);
-					} else {
-						_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1, false);
+		if (!world.isClientSide()) {
+			if (blockstate.getBlock() == PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get()) {
+				if (entity instanceof Player _player)
+					_player.getCooldowns().addCooldown(itemstack.getItem(), (int) ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 10 : 20));
+				if (Math.random() < ((entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(PrimogemcraftModItems.HQBING.get())) : false) ? 1 : 0.95)) {
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1);
+						} else {
+							_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.beacon.power_select")), SoundSource.BLOCKS, 1, 1, false);
+						}
 					}
-				}
-				if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.ZIYOUSONGSHIKUAI.get().defaultBlockState(), 3);
-				} else if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.JIANLAOHUANGYUKUAI.get().defaultBlockState(), 3);
-				} else if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.ZUISNEGZIJINGKUAI.get().defaultBlockState(), 3);
-				} else if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.SHENGZHANGBIFEIDAKUAI.get().defaultBlockState(), 3);
-				} else if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.DIJINGQINGJINKUAI.get().defaultBlockState(), 3);
-				} else if (Math.random() < 0.18) {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.RANYUANKUAI_2.get().defaultBlockState(), 3);
+					world.setBlock(BlockPos.containing(x, y, z),
+							(BuiltInRegistries.BLOCK.getOrCreateTag(BlockTags.create(ResourceLocation.parse("c:yuansujingtikuai"))).getRandomElement(RandomSource.create()).orElseGet(() -> BuiltInRegistries.BLOCK.wrapAsHolder(Blocks.AIR)).value())
+									.defaultBlockState(),
+							3);
 				} else {
-					world.setBlock(BlockPos.containing(x, y, z), PrimogemcraftModBlocks.AIXUBINGYUFANGKU_I.get().defaultBlockState(), 3);
+					world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+					world.levelEvent(2001, BlockPos.containing(x, y, z), Block.getId(PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get().defaultBlockState()));
 				}
-			} else {
-				world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-				world.levelEvent(2001, BlockPos.containing(x, y, z), Block.getId(PrimogemcraftModBlocks.YUANSUJINGSHIKUAI.get().defaultBlockState()));
 			}
 		}
 	}
