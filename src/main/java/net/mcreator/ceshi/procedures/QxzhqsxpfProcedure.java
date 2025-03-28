5d40483b0bebc03084bbb2054b0a3df54832b325
Pepper MR <zheshixbde@outlook.com>
2025-03-29 02:25:41 +0800

    炸了，修复

--- src/main/java/net/mcreator/ceshi/procedures/QxzhqsxpfProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/QxzhqsxpfProcedure.java
@@ -12,11 +12,10 @@ import net.mcreator.ceshi.init.PrimogemcraftModItems;
 public class QxzhqsxpfProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		boolean o1 = false;
-		if (!ModList.get().isLoaded("genshincraft")) {
-			QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(PrimogemcraftModItems.XINGHUI.get()), new ItemStack(PrimogemcraftModItems.XINGCHEN.get()), 1, 1, 2, 1);
-			QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(PrimogemcraftModItems.XINGCHEN.get()), new ItemStack(PrimogemcraftModItems.XINGHUI.get()), 1, 3, 1, 2);
-		} else {
-			QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(BuiltInRegistries.ITEM.get(ResourceLocation.parse("genshincraft:primogem"))), new ItemStack(PrimogemcraftModItems.YUANSHI.get()), 2, 1, 6, 1);
+		QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(PrimogemcraftModItems.XINGHUI.get()), new ItemStack(PrimogemcraftModItems.XINGCHEN.get()), 1, 1, 2, 1);
+		QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(PrimogemcraftModItems.XINGCHEN.get()), new ItemStack(PrimogemcraftModItems.XINGHUI.get()), 1, 3, 1, 2);
+		if (ModList.get().isLoaded("genshincraft")) {
+			QxzhqsxhsProcedure.execute(world, x, y, z, new ItemStack(BuiltInRegistries.ITEM.get(ResourceLocation.parse("genshincraft:primogems"))), new ItemStack(PrimogemcraftModItems.YUANSHI.get()), 2, 1, 6, 1);
 		}
 	}
 }
